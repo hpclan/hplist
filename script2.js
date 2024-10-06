d39488e38e69c1aab451346534d54b777b7f803b
hpclan <zonaelneidaitona@gmail.com>
2024-10-06 21:10:58 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,21 @@
+function toggleMenu() {
+    var menu = document.getElementById("dropdown");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+// Cerrar el menú si el usuario hace clic fuera del mismo
+window.onclick = function(event) {
+    if (!event.target.matches('.menu-btn')) {
+        var dropdowns = document.getElementsByClassName("menu");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
